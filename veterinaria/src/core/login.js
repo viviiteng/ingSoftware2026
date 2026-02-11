5a1842924c30a2d4e20833ed16aba675a848ea91
Takeshi Gomez <takegomez03@gmail.com>
2026-02-10 22:16:18 -0300

    mensajes de error

--- veterinaria/src/core/login.js
+++ veterinaria/src/core/login.js
@@ -7,7 +7,7 @@ const USER_CREDENTIALS = {
 
 const form = document.getElementById("loginForm");
 const errorMsg = document.getElementById("errorMsg");
-
+const errorMsg2 = document.getElementById("errorMsg2");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
@@ -28,6 +28,8 @@ form.addEventListener("submit", function (e) {
 
         // Redirigir
         window.location.href = "../html/admin.html";
+    }else if( userInput == "" || passwordInput == "") {
+         errorMsg2.style.display = "flex";
     } else {
         // Mostrar error
         errorMsg.style.display = "flex";
